a7f531d3507275b9a435ea8633f1f09a4953a3d0
vantoan1511 <vantoan1517@gmail.com>
2024-04-10 22:25:12 +0700

    implement endpoints serve - retrieving all logs as list - deleting a single log & multiple logs

--- src/main/java/com/iotproject/api/MoistureAPI.java
+++ src/main/java/com/iotproject/api/MoistureAPI.java
@@ -1,9 +1,8 @@
-package com.iotproject.controller;
+package com.iotproject.api;
 
 import com.iotproject.entity.Moisture;
 import com.iotproject.http.ErrorResponse;
 import com.iotproject.service.IMoistureService;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -12,20 +11,27 @@ import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/api/moisturelogs")
-public class MoistureController {
+public class MoistureAPI {
 
     private final IMoistureService moistureService;
 
-    public MoistureController(IMoistureService moistureService) {
+    public MoistureAPI(IMoistureService moistureService) {
         this.moistureService = moistureService;
     }
 
     @GetMapping({"/", ""})
-    public Page<Moisture> getAllLogs(@RequestParam(value = "page", defaultValue = "1") int page,
-                                     @RequestParam(value = "size", defaultValue = "10") int size) {
+    public Object getAllLogs(
+            @RequestParam(value = "unpaged", defaultValue = "false") boolean unPaged,
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size
+    ) {
+        if (unPaged) {
+            return moistureService.getAllLogs();
+        }
         Pageable pageRequest = PageRequest.of(page - 1, size);
         return moistureService.getAllLogs(pageRequest);
     }
@@ -56,6 +62,38 @@ public class MoistureController {
                     .message(ex.getLocalizedMessage())
                     .timestamp(Timestamp.from(Instant.now()))
                     .build(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(
+            @PathVariable("id") long id
+    ) {
+        try {
+            moistureService.delete(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return new ResponseEntity<>(ErrorResponse.builder()
+                    .path("/v1/api/moisturelogs")
+                    .code(HttpStatus.NOT_FOUND.value())
+                    .message(e.getLocalizedMessage())
+                    .timestamp(Timestamp.from(Instant.now()))
+                    .build(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> delete(@RequestBody List<Long> ids) {
+        try {
+            moistureService.delete(ids);
+            return ResponseEntity.ok("Deleted successfully!");
+        } catch (Exception ex) {
+            return new ResponseEntity<>(ErrorResponse.builder()
+                    .path("/v1/api/moisturelogs")
+                    .code(HttpStatus.NOT_FOUND.value())
+                    .message(ex.getLocalizedMessage())
+                    .timestamp(Timestamp.from(Instant.now()))
+                    .build(), HttpStatus.NOT_FOUND);
         }
     }
 }
--- src/main/java/com/iotproject/api/TemperatureAPI.java
+++ src/main/java/com/iotproject/api/TemperatureAPI.java
@@ -1,9 +1,8 @@
-package com.iotproject.controller;
+package com.iotproject.api;
 
 import com.iotproject.entity.Temperature;
 import com.iotproject.http.ErrorResponse;
 import com.iotproject.service.ITemperatureService;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -12,20 +11,27 @@ import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/api/templogs")
-public class TemperatureController {
+public class TemperatureAPI {
 
     private final ITemperatureService temperatureService;
 
-    public TemperatureController(ITemperatureService temperatureService) {
+    public TemperatureAPI(ITemperatureService temperatureService) {
         this.temperatureService = temperatureService;
     }
 
     @GetMapping({"/", ""})
-    public Page<Temperature> getAllLogs(@RequestParam(value = "page", defaultValue = "1") int page,
-                                        @RequestParam(value = "size", defaultValue = "10") int size) {
+    public Object getAllLogs(
+            @RequestParam(value = "unpaged", defaultValue = "false") boolean unPaged,
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size
+    ) {
+        if (unPaged) {
+            return temperatureService.getAllLogs();
+        }
         Pageable pageRequest = PageRequest.of(page - 1, size);
         return temperatureService.getAllLogs(pageRequest);
     }
@@ -56,6 +62,36 @@ public class TemperatureController {
                     .message(ex.getLocalizedMessage())
                     .timestamp(Timestamp.from(Instant.now()))
                     .build(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") long id) {
+        try {
+            temperatureService.delete(id);
+            return ResponseEntity.ok("Deleted successfully!");
+        } catch (RuntimeException ex) {
+            return new ResponseEntity<>(ErrorResponse.builder()
+                    .path("/v1/api/templogs")
+                    .code(HttpStatus.NOT_FOUND.value())
+                    .message(ex.getLocalizedMessage())
+                    .timestamp(Timestamp.from(Instant.now()))
+                    .build(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> delete(@RequestBody List<Long> ids) {
+        try {
+            temperatureService.delete(ids);
+            return ResponseEntity.ok("Deleted successfully!");
+        } catch (RuntimeException ex) {
+            return new ResponseEntity<>(ErrorResponse.builder()
+                    .path("/v1/api/templogs")
+                    .code(HttpStatus.NOT_FOUND.value())
+                    .message(ex.getLocalizedMessage())
+                    .timestamp(Timestamp.from(Instant.now()))
+                    .build(), HttpStatus.NOT_FOUND);
         }
     }
 }
